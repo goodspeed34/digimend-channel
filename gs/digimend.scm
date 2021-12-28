8ebfbace4edfeb4a869a44b6b38836de0f554802
Williams Goodspeed <goodspeed@anche.no>
2021-12-28 09:00:45 +0800

    DIGImend package

---
+++ gs/digimend.scm
@@ -0,0 +1,18 @@
+(define-public digimend-linux-module
+  (package
+   (name "digimend-linux-module")
+   (version "10.0.0")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (string-append "https://github.com/goodspeed34/digimend-kernel-drivers/archive/refs/tags/v" version ".tar.gz"))
+     (sha256
+      (base32 "10zwx8cinfzrjcgfamxs3fhqrllsyj3ds26wax8xpylnqf2aifr4"))))
+   (build-system linux-module-build-system)
+   (arguments
+    `(#tests? #f)) ; No test avaiable
+   (home-page "https://github.com/goodspeed34")
+   (synopsis "DIGImend graphics tablet drivers for the Linux kernel")
+   (description
+    "This is a collection of graphics tablet drivers for the Linux kernel, produced and maintained by the DIGImend project. We maintain this package to provide newer drivers for older kernel versions which don't have them, and to allow users to test new drivers before we contribute them to the mainline kernel.")
+   (license license:gpl2)))
