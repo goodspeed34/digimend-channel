8da8868011847dbcdc7b5158c6a6c32ec183c2e0
Williams Goodspeed <goodspeed@anche.no>
2022-01-07 13:00:25 +0800

    add udev

--- gs/digimend.scm
+++ gs/digimend.scm
@@ -4,19 +4,20 @@
   #:use-module (guix packages)
   #:use-module (guix build utils)
   #:use-module (guix build-system linux-module)
+  #:use-module (guix build-system copy)
   #:export (gs digimend))
 
 
 (define-public digimend-linux-module
   (package
    (name "digimend-linux-module")
-   (version "10.0.1")
+   (version "10.0.4")
    (source
     (origin
      (method url-fetch)
      (uri (string-append "https://github.com/goodspeed34/digimend-kernel-drivers/archive/refs/tags/v" version ".tar.gz"))
      (sha256
-      (base32 "02my36xvh52rw7cnbknscnfrhxg16hwqik37xbajj3y10lcwms9j"))))
+      (base32 "14ib0yhm0bcrbjlk0qvjl4ma4jy3nnighd5a4pkhbl4826dyfdw1"))))
    (build-system linux-module-build-system)
    (arguments
     `(#:tests? #f)) ; No test avaiable
@@ -24,4 +25,24 @@
    (synopsis "DIGImend graphics tablet drivers for the Linux kernel")
    (description
     "This is a collection of graphics tablet drivers for the Linux kernel, produced and maintained by the DIGImend project. We maintain this package to provide newer drivers for older kernel versions which don't have them, and to allow users to test new drivers before we contribute them to the mainline kernel.")
-   (license license:gpl2)))
+   (license license:gpl2+)))
+
+(define-public digimend-udev-rules
+  (package
+   (name "digimend-udev-rules")
+   (version "10.0.4")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (string-append "https://github.com/goodspeed34/digimend-kernel-drivers/archive/refs/tags/v" version ".tar.gz"))
+     (sha256
+      (base32 "14ib0yhm0bcrbjlk0qvjl4ma4jy3nnighd5a4pkhbl4826dyfdw1"))))
+   (build-system copy-build-system)
+   (arguments
+    '(#:install-plan
+      '(("./udev.rules" "lib/udev/rules.d/90-digimend.rules")
+	("./hid-rebind", "lib/udev/hid-rebind"))))
+   (synopsis "DIGImend graphics tablet drivers for the Linux kernel")
+   (description
+    "This is a collection of graphics tablet drivers for the Linux kernel, produced and maintained by the DIGImend project. We maintain this package to provide newer drivers for older kernel versions which don't have them, and to allow users to test new drivers before we contribute them to the mainline kernel.")
+   (license license:gpl2+)))
